9696f1031754e4ce034694e65be9eb257f55e07c
SK0P1 <45910581+SK0P1@users.noreply.github.com>
2020-10-22 16:35:53 -0700

    Update firstproject.cpp

--- firstproject.cpp
+++ firstproject.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 
-//hi
+int main() {
+  int numbers[5];
+  for (int i=0; i<5; i++){
+    int input;
+      std::cout <<"Input Number: ";
+      std::cin >>input;
+        if ( input % 2 == 0){
+          numbers[i]=input;
+        }
+        else{
+          numbers[i] = 0;
+        }
+  int sum = 0;
+  for (int i = 0; i < 5; i++) {
+    sum = sum + numbers[i];
+}
+  std::cout << sum << std::endl;
+  }
+}
